0fec75afeee5086b2ab95a03a2fd7c3f83ca43fa
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-04-23 18:34:44 -0700

    fix(dependencies): Downgrade Tailwind 4 back to version 3.4.15 and comment out development navigation tools

--- app/(routes)/exam/page.tsx
+++ app/(routes)/exam/page.tsx
@@ -652,7 +652,7 @@ export default function ExamHomePage() {
             </Stack>
           </main>
         )}
-        {viewState !== VIEW_STATES.SUBMIT_AND_EXIT &&
+        {/* {viewState !== VIEW_STATES.SUBMIT_AND_EXIT &&
           viewState !== VIEW_STATES.START_TEST && (
             <Stack spacing={4}>
               <Button onClick={incrementViewState}>
@@ -670,7 +670,7 @@ export default function ExamHomePage() {
                 <Typography>{"Print Data"}</Typography>
               </Button>
             </Stack>
-          )}
+          )} */}
       </Stack>
     </Box>
   );
