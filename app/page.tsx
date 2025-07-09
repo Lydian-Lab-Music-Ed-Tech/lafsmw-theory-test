e25c89135158c9cffe2268c6b10338dae5d750f2
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-07-08 17:02:30 -0700

    feat(tempLandingPage): Create new password protected landing page state on the Home page. Add textAlign attribute to the FormInput component and update the FormInputProps type to include textAlign options.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import SignInForm from "./components/SignInForm";
 import SignUpForm from "./components/SignUpForm";
+import FormInput from "./components/FormInput";
 
 export default function Home() {
   const { user } = useAuthContext();
@@ -12,6 +13,8 @@ export default function Home() {
   const [isSignUp, setIsSignUp] = useState(false);
   const [isSignIn, setIsSignIn] = useState(false);
   const [showButtons, setShowButtons] = useState(true);
+  const [isUnreleased, setIsUnreleased] = useState(true);
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     if (user !== null) {
@@ -35,6 +38,18 @@ export default function Home() {
     setIsSignUp(false);
   };
 
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const checkPassword = () => {
+    if (password === "tacotuesday") {
+      setIsUnreleased(false);
+    } else {
+      setIsUnreleased(true);
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -46,41 +61,74 @@ export default function Home() {
         p: 6,
       }}
     >
-      <Stack spacing={8}>
-        {showButtons && (
-          <Stack spacing={2}>
-            <Typography>New to LAFSMW? Sign up here:</Typography>
-            <Button variant="contained" onClick={toggleIsSignUp}>
-              Sign Up
+      {isUnreleased && (
+        <Stack spacing={8} marginTop={8}>
+          <Typography variant="h5" align="center">
+            Welcome to the official site of the Lafayette Jazz Workshop Theory
+            Exam!
+          </Typography>
+          <Typography variant="body1" align="center">
+            This exam has not been officially released to students yet. If you
+            have been authorized to take this test, please enter the password
+            given to you by the instructor.
+          </Typography>
+          <Stack direction="row" spacing={2} justifyContent="center">
+            <FormInput
+              labelText="Enter Password:"
+              name="password"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              width="100%"
+              textAlign="left"
+            />
+            <Button
+              variant="text"
+              onClick={checkPassword}
+              sx={{ width: "100px", borderRadius: "50px" }}
+            >
+              Submit
             </Button>
           </Stack>
-        )}
+        </Stack>
+      )}
+      {!isUnreleased && (
+        <Stack spacing={8}>
+          {showButtons && (
+            <Stack spacing={2}>
+              <Typography>New to LAFSMW? Sign up here:</Typography>
+              <Button variant="contained" onClick={toggleIsSignUp}>
+                Sign Up
+              </Button>
+            </Stack>
+          )}
 
-        {showButtons && (
-          <Stack spacing={2}>
-            <Typography>Returning student? Sign in here:</Typography>
-            <Button variant="contained" onClick={toggleIsSignIn}>
-              Sign In
-            </Button>
-          </Stack>
-        )}
-        {isSignUp && (
-          <Grid container justifyContent="center" gap={4}>
-            <SignUpForm />
-            <Button variant="text" onClick={goBack} sx={{ width: "73%" }}>
-              Go Back
-            </Button>
-          </Grid>
-        )}
-        {isSignIn && (
-          <Grid container justifyContent="center" gap={4}>
-            <SignInForm />
-            <Button variant="text" onClick={goBack} sx={{ width: "73%" }}>
-              Go Back
-            </Button>
-          </Grid>
-        )}
-      </Stack>
+          {showButtons && (
+            <Stack spacing={2}>
+              <Typography>Returning student? Sign in here:</Typography>
+              <Button variant="contained" onClick={toggleIsSignIn}>
+                Sign In
+              </Button>
+            </Stack>
+          )}
+          {isSignUp && (
+            <Grid container justifyContent="center" gap={4}>
+              <SignUpForm />
+              <Button variant="text" onClick={goBack} sx={{ width: "73%" }}>
+                Go Back
+              </Button>
+            </Grid>
+          )}
+          {isSignIn && (
+            <Grid container justifyContent="center" gap={4}>
+              <SignInForm />
+              <Button variant="text" onClick={goBack} sx={{ width: "73%" }}>
+                Go Back
+              </Button>
+            </Grid>
+          )}
+        </Stack>
+      )}
     </Box>
   );
 }
